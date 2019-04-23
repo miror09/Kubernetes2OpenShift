d02f77abb5220c855dce3be69d160a830c1bbbd2
miror09 <31731301+miror09@users.noreply.github.com>
2019-04-23 11:28:42 +0200

    Update app.py

--- bookshop/app.py
+++ bookshop/app.py
@@ -4,11 +4,11 @@ import requests
 
 
 app = Flask(__name__)
-BOOKS = ['1984', 'foo', 'bar', "ahoj a", "miror"]
+BOOKS = ['1984', 'foo', 'bar', "ahoj a", "miror", "libor"]
 
 @app.route("/")
 def hello():
-    response = "<H1>Books PrgCont LIBOR:</H1><BR>"
+    response = "<H1>Books PrgCont MIROR:</H1><BR>"
     for book in BOOKS:
         try:
             review = requests.get(f'http://bookreview:8080/v1/review/{book}')
